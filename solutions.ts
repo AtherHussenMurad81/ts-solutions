b3ad009cb0d0b70399798f5ebf4b6374434cf9ec
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-05-06 23:01:11 +0600

    class complete

--- solutions.ts
+++ solutions.ts
@@ -4,7 +4,6 @@ function filterEvenNumbers(numbers: number[]) {
   const result = numbers.filter((number) => number % 2 == 0);
   return result;
 }
-// filterEvenNumbers([1, 2, 3, 4, 5, 6]);
 
 // * Problem 2
 
@@ -13,7 +12,6 @@ function reverseString(reverseNumbers: string): string {
 
   return result;
 }
-reverseString("typescript");
 
 // * Problem 3
 
@@ -26,4 +24,40 @@ function checkType(value: StringOrNumber) {
     return "Number";
   }
 }
-const result = checkType(545);
+
+// * Problem 4
+
+const getProperty = <X>(object: X, key: keyof X) => {
+  return object[key];
+};
+
+//*  Problem 5
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+
+function toggleReadStatus(book: Book & { isRead: boolean }) {
+  return {
+    ...book,
+    isRead: true,
+  };
+}
+
+// * Problem 6
+
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+class Student {
+
+
+    
+}
